4a744f2264c5131c443269608e05ea3d676cf8ac
Megsie Siple <mcsiple@gmail.com>
2020-07-10 16:00:16 -0700

    Updated to most recent version from shinymammals

--- R/NPR.R
+++ R/NPR.R
@@ -5,9 +5,13 @@
 #' @param S1plus adult survival (num)
 #' @param nages plus group age (num)
 #' @param AgeMat age at maturity (num)
-#' @param f 'fishing' rate == bycatch rate (num)
+#' @param f bycatch rate (could be E; exploitation rate) (num)
 #' @return A list of numbers per recruit (npr), 1+ numbers per recruit (P1r), and numbers at age per recruit (nvec)
 #'
+#' @examples
+#' (unpr <- NPR(S0 = 0.9,S1plus = 0.9,AgeMat=11, nages = 13,f=0))     # unfished nums per recruit
+#' (nprf <- NPR(S0 = 0.9,S1plus = 0.9,AgeMat=11, nages = 13,f=0.8))   # nums per recruit at F
+
 #' @export
 NPR <- function(S0, S1plus, nages, AgeMat, f=0){
   AgePart <- AgeMat+1 # Age at first parturition
@@ -24,8 +28,8 @@ NPR <- function(S0, S1plus, nages, AgeMat, f=0){
   }
 
   N.vec[nages+1] <- (S0 * OnePlusSurv^(nages-1) ) / (1-OnePlusSurv) # plus group age
-  npr <- sum(N.vec[(AgePart+1):(nages+1)]) # REPRODUCING females (AgeMat+1 = age at first parturition)
-  P1r <- sum(N.vec[2:(nages+1)]) # 1+ whales
+  npr <- sum(N.vec[(AgePart+1):(nages+1)]) # REPRODUCING animals (AgeMat+1 = age at first parturition)
+  P1r <- sum(N.vec[2:(nages+1)]) # 1+ whales/pinnipeds
   Outs <- list()
   Outs$npr <- npr
   Outs$P1r <- P1r
