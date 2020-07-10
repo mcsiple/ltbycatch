7b8ab8e381603e8f4071d9846ab74ee902cfc4cd
Megsie Siple <mcsiple@gmail.com>
2020-07-10 15:24:46 -0700

    FIXED getRF() to new version with P0, N0

--- R/getRF.R
+++ R/getRF.R
@@ -9,26 +9,26 @@
 #' @param AgeMat Age at maturity (num)
 #' @param z degree of compensation (num)
 #' @param A Pella-Tomlinson resilience parameter (Punt 1999; Annex R). A = (FecMax - Fec0) / Fec0 (num)
-#' @param unpr Unfished numbers-per-recruit (num)
+#' @param P0 Unfished 1+ numbers per recruit, \tildeP(0). Must double-check with AEP.
+#' @param N0 Unfished mature numbers per recruit, \tildeN(0). Must double-check with AEP.
 #' @return recruitment given exploitation rate E - to multiply by Ninit to get initial nums at age (num)
 #'
 #' @export
-getRF <- function(E, S0, S1plus, nages,K1plus, AgeMat, z, A, unpr){ #I have remvoed lambdaMax because it's only used here to calculate A, which is now done outside the fn!
+getRF <- function(E, S0, S1plus, nages,K1plus, AgeMat, z, A, P0, N0){
 
-  nprf <- NPR(S0 = S0,S1plus = S1plus,nages = nages, AgeMat = AgeMat, f=E)$npr
+  NE <- NPR(S0 = S0,S1plus = S1plus,nages = nages, AgeMat = AgeMat, f=E)$npr
+  PE <- NPR(S0 = S0,S1plus = S1plus,nages = nages, AgeMat = AgeMat, f=E)$P1r
 
   # More general version ('actual' R0 = K1plus/P1r)
   R0 <- 1
 
   # Fecundity at unfished equilibrium
-  # AEP: You could pass FecMax and A into the function because all you are changing is E
-  # MCS: I don't use FecMax except to calculate A so the fn now just passes A
-  Fec0 = 1.0/unpr
+  Fec0 = 1.0/N0
 
-  if ((1-Fec0*nprf)/(Fec0*nprf*A) > 1)
+  if ((1-Fec0*NE)/(Fec0*NE*A) > 1)
     R.F <- 0
   else
-    R.F <- ( 1 - (1-Fec0*nprf)/(Fec0*nprf*A))^(1/z) * ((R0*unpr)/nprf)
+    R.F <- ( 1 - (1-Fec0*NE)/(Fec0*NE*A))^(1/z) * ((R0*P0)/PE) # this used to be NE!
 
   return(R.F)
 }
