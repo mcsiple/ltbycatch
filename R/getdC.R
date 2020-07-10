acef1e5cdc7fce5346df6f0e870281e67d6b5cb8
Megsie Siple <mcsiple@gmail.com>
2020-07-10 16:14:19 -0700

    add getdC

---
+++ R/getdC.R
@@ -0,0 +1,41 @@
+#' Calculate the derivative of C in getMNPL()
+#'
+#' @description this is the function to minimize, but needs to be restricted to that yield > 0
+#' All life history params are as above
+#' @param logit.E logit transform of exploitation rate
+#' @param S0 calf/pup survival
+#' @export
+getdC <- function(logit.E,S0=S0.w, S1plus=S1plus.w, nages=nages.w, AgeMat=AgeMat.w, lambdaMax=lambdaMax.w, K1plus=K1plus.w, z=z.w){
+  exploitation.rate <- inv.logit(logit.E)
+  h <- 0.000001
+
+  NPROut <- NPR(S0 = S0,S1plus = S1plus,nages = nages, AgeMat = AgeMat, f=0)
+  N0 <- NPROut$npr
+  P0 <- NPROut$P1r
+  Fec0 <- 1.0/N0
+  fmax <- getFecmax2(lambdaMax = lambdaMax,S0 = S0,S1plus = S1plus,AgeMat = AgeMat)
+  A <- (fmax - Fec0) / Fec0
+
+  # approximate the derivative by evaluating cE() at a slightly higher and slightly lower E
+  C1 <- cE(S0 = S0,
+           S1plus = S1plus,
+           nages = nages,
+           K1plus = K1plus,
+           AgeMat = AgeMat,
+           z = z,
+           lambdaMax =lambdaMax,
+           E = exploitation.rate+h,
+           A = A,P0 = P0,N0 = N0)/(2*h)
+  C2 <- cE(S0 = S0,
+           S1plus = S1plus,
+           nages = nages,
+           K1plus = K1plus,
+           AgeMat = AgeMat,
+           z = z,
+           lambdaMax =lambdaMax,
+           E = exploitation.rate-h,
+           A = A,P0 = P0,N0 = N0)/(2*h)
+
+  dC <- C1-C2 #diff
+  return(dC)
+}
